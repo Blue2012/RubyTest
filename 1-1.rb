19daab11567a4d697bfdc9eb401ca50814171ce5
Kenzo Ookane <kanechanee@gmail.com>
2020-02-02 23:43:02 +0900

    2020/02/02 ファイル追加

---
+++ 1-1.rb
@@ -0,0 +1,8 @@
+
+str1="string"
+str2="string"
+
+str1.object_id	#=> 84640: ある文字列オブジェクト
+str2.object_id	#=> 84620: 異なる文字列オブジェクト
+str1 == str2 #=> true: 内容は同じ
+str1.equal?(str2) #=> false: 内容は同じでもオブジェクトとしては異なる
