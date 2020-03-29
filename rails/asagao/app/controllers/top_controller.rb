e58bbc6b03d534f8f1488c3d39443e6cf5061df4
BLUE2012 <2012blue2012@gmail.com>
2020-03-29 21:21:00 +0900

    20200329Mod2

--- rails/asagao/app/controllers/top_controller.rb
+++ rails/asagao/app/controllers/top_controller.rb
@@ -2,4 +2,11 @@ class TopController < ApplicationController
   def index
     @message = "おはようございます！"
   end
+
+  def about
+  end
+  
+  def kanechanee
+    @kanechanee = "Hello,World!"
+  end
 end
--- rails/asagao/config/routes.rb
+++ rails/asagao/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "top#index"
+  get "about" => "top#about"
+  get "kanechanee" => "top#kanechanee"
 end
